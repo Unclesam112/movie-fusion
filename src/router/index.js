5f016b9d08d823ab63abda576fc989d158becb12
Engineer Adeyemi <66875174+Unclesam112@users.noreply.github.com>
2023-11-12 11:47:41 -0800

    new

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
-import movieDetail from '../views/movieDetails.vue'
+import movieDetail from '../views/MovieDetails.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
